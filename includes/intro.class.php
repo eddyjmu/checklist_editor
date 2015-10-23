fdbc6398e7a15e8a9e751a8c55ec9302723f4963
mullere <edward.j.muller@gmail.com>
2015-10-23 08:41:51 -0400

    Update intro.class.php

--- includes/intro.class.php
+++ includes/intro.class.php
@@ -1,8 +1,10 @@
 <?php
 	class intro {
-		public static $page_title;
+		public $page_title;
+		public $page_content;
 		function __construct(){
-			self::$page_title = 'Home';
+			$this->page_title = 'Home';
+			$this->page_content = 'Hello and welcome to Checklist Editor.</br><i>Edward Muller - October 2015</i>';
 		}
 	}
 ?>

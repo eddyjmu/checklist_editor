1a79f8b4db69fa643fe2bccacaa6629bdfa7dc7c
mullere <edward.j.muller@gmail.com>
2015-10-23 08:42:57 -0400

    Update page.class.php

--- includes/page.class.php
+++ includes/page.class.php
@@ -1,23 +1,30 @@
 <?php
 	class page {
 		public $model;
-		private $settings;
-		public function __construct($model){
+		public $page_sought;
+		public function __construct($page_sought,$model){
 			$this->model = $model;
+			$this->page_sought = $page_sought;
 		}
 		public function menu(){
 			$menu = '';
 			foreach(settings::$page_arrays as $page_get => $page_array){
-				$menu .= ''.$page_get.''.$page_array['title'].'';
+				$menu .= '<a href="?page='.$page_get.'">';
+				if($page_get == $this->page_sought){
+					$menu .= '['.$page_array['title'].']';
+				} else {
+					$menu .= $page_array['title'];
+				}
+				$menu .= '</a> ';
 			}
 			$menu .= '';
 			return $menu;
 		}
 		public function output(){
-			$page_content = '<html><head><title>'.$this->model->page_title.'</title><head><body>';
-			$page_content .= '';
-			$page_content .= $this->menu();
-			$page_content .= '';
+			$page_content = '<html><head><title>'.$this->model->page_title.' | '.settings::$site_title.'</title><head><body>';
+			$page_content .= '<h3>'.settings::$site_title.'</h3>';
+			$page_content .= $this->menu().'</br></br>';
+			$page_content .= $this->model->page_content;
 			return $page_content;
 		}
 	}

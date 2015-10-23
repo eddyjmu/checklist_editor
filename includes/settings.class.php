8b12c764b07f04476c2e41ba082966c5a9c81ea8
mullere <edward.j.muller@gmail.com>
2015-10-22 20:32:59 -0400

    Update settings.class.php

--- includes/settings.class.php
+++ includes/settings.class.php
@@ -1,10 +1,12 @@
 <?php
 	class settings {
 		public $page_arrays = array(
-			'home' => array('title' => 'Home','model' => 'intro','view' => 'page','controller' => 'home'),
+			'home' => array('title' => 'Home','model' => 'intro','view' => 'page','controller' => 'layout'),
 			'new' => array('title' => 'New List','model' => '','view' => 'class_page','controller' => 'class_new_list'),
 			'edit' => array('title' => 'Edit Lists','model' => '','view' => 'class_page','controller' => 'class_edit'),
 			'options' => array('title' => 'Options','model' => '','view' => 'class_page','controller' => 'class_options'),
 			'reports' => array('title' => 'Reports','model' => '','view' => 'class_page','controller' => 'class_report'),
 		);
+		public $includes_folder = 'includes';
 	}
+?>

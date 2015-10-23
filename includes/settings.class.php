8382639b811f8c990a1e7d6bdd8d5d7ea2e8f66c
mullere <edward.j.muller@gmail.com>
2015-10-22 22:57:53 -0400

    Update settings.class.php

--- includes/settings.class.php
+++ includes/settings.class.php
@@ -1,12 +1,17 @@
 <?php
 	class settings {
-		public $page_arrays = array(
-			'home' => array('title' => 'Home','model' => 'intro','view' => 'page','controller' => 'layout'),
-			'new' => array('title' => 'New List','model' => '','view' => 'class_page','controller' => 'class_new_list'),
-			'edit' => array('title' => 'Edit Lists','model' => '','view' => 'class_page','controller' => 'class_edit'),
-			'options' => array('title' => 'Options','model' => '','view' => 'class_page','controller' => 'class_options'),
-			'reports' => array('title' => 'Reports','model' => '','view' => 'class_page','controller' => 'class_report'),
-		);
-		public $includes_folder = 'includes';
+		public static $page_arrays;
+		public static $includes_folder;
+		public function __construct(){
+			self::$page_arrays = array(
+				'home' => array('title' => 'Home','model' => 'intro','view' => 'page','controller' => 'layout'),
+				'new' => array('title' => 'New List','model' => '','view' => 'class_page','controller' => 'class_new_list'),
+				'edit' => array('title' => 'Edit Lists','model' => '','view' => 'class_page','controller' => 'class_edit'),
+				'options' => array('title' => 'Options','model' => '','view' => 'class_page','controller' => 'class_options'),
+				'reports' => array('title' => 'Reports','model' => '','view' => 'class_page','controller' => 'class_report'),
+			);
+			self::$includes_folder = 'includes';
+		}
+
 	}
 ?>

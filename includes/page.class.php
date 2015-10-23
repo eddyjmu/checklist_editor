8e752ffc43943c54cdaf20319b499526e71c8314
mullere <edward.j.muller@gmail.com>
2015-10-22 22:57:19 -0400

    Update page.class.php

--- includes/page.class.php
+++ includes/page.class.php
@@ -1,19 +1,20 @@
 <?php
 	class page {
-		private $model;
+		public $model;
+		private $settings;
 		public function __construct($model){
 			$this->model = $model;
 		}
 		public function menu(){
 			$menu = '';
-			foreach($settings->page_arrays as $page_get => $page_array){
+			foreach(settings::$page_arrays as $page_get => $page_array){
 				$menu .= ''.$page_get.''.$page_array['title'].'';
 			}
 			$menu .= '';
 			return $menu;
 		}
 		public function output(){
-			$page_content = '<html><head><title>page working</title><head><body>';
+			$page_content = '<html><head><title>'.$this->model->page_title.'</title><head><body>';
 			$page_content .= '';
 			$page_content .= $this->menu();
 			$page_content .= '';

27a1d376a727071f9c8ccbec324ba678937e40ea
mullere <edward.j.muller@gmail.com>
2015-10-23 08:43:24 -0400

    Update settings.class.php

--- includes/settings.class.php
+++ includes/settings.class.php
@@ -2,15 +2,17 @@
 	class settings {
 		public static $page_arrays;
 		public static $includes_folder;
+		public static $site_title;
 		public function __construct(){
 			self::$page_arrays = array(
 				'home' => array('title' => 'Home','model' => 'intro','view' => 'page','controller' => 'layout'),
-				'new' => array('title' => 'New List','model' => '','view' => 'class_page','controller' => 'class_new_list'),
-				'edit' => array('title' => 'Edit Lists','model' => '','view' => 'class_page','controller' => 'class_edit'),
-				'options' => array('title' => 'Options','model' => '','view' => 'class_page','controller' => 'class_options'),
-				'reports' => array('title' => 'Reports','model' => '','view' => 'class_page','controller' => 'class_report'),
+				'new' => array('title' => 'New List','model' => 'newlist','view' => 'page','controller' => 'layout'),
+				'edit' => array('title' => 'Edit Lists','model' => 'editlist','view' => 'page','controller' => 'layout'),
+				'options' => array('title' => 'Options','model' => 'optionspage','view' => 'page','controller' => 'layout'),
+				'reports' => array('title' => 'Reports','model' => 'reportspage','view' => 'page','controller' => 'layout'),
 			);
 			self::$includes_folder = 'includes';
+			self::$site_title = 'Checklist Editor';
 		}
 
 	}

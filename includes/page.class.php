9661d5d2c8d15528cfc804fb974ccf39e4467e47
mullere <edward.j.muller@gmail.com>
2015-10-22 20:33:21 -0400

    Update page.class.php

--- includes/page.class.php
+++ includes/page.class.php
@@ -1,22 +1,23 @@
 <?php
-class page {
-	private $model
-	public function __construct($model){
-		$this->model = $model;
-	}
-	public function menu(){
-		$menu = '';
-		foreach($settings->page_arrays as $page_get => $page_array){
-			$menu .= ''.$page_get.''.$page_array['title'].'';
+	class page {
+		private $model;
+		public function __construct($model){
+			$this->model = $model;
 		}
-		$menu .= '';
-		return $menu;
+		public function menu(){
+			$menu = '';
+			foreach($settings->page_arrays as $page_get => $page_array){
+				$menu .= ''.$page_get.''.$page_array['title'].'';
+			}
+			$menu .= '';
+			return $menu;
+		}
+		public function output(){
+			$page_content = '<html><head><title>page working</title><head><body>';
+			$page_content .= '';
+			$page_content .= $this->menu();
+			$page_content .= '';
+			return $page_content;
+		}
 	}
-	public function output(){
-		$page_content = '';
-		$page_content .= '';
-		$page_content .= $this->menu();
-		$page_content .= '';
-		return $page_content;
-	}
-}
+?>

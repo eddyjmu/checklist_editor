ebf72ca581281fb05cb8d0766111585ecf8bbd3b
mullere <edward.j.muller@gmail.com>
2015-10-24 18:32:39 -0400

    Update optionspage.class.php

--- includes/optionspage.class.php
+++ includes/optionspage.class.php
@@ -1,10 +1,18 @@
 <?php
 	class optionspage {
+		public $page_call;
 		public $page_title;
+		public $page_menu;
 		public $page_content;
+		public $dependencies;
+		public $view;
+		public $controllers;
 		function __construct(){
+			$this->page_call = 'optionspage';
 			$this->page_title = 'Options';
 			$this->page_content = '';
+			$this->view = 'optionspagelayout';
+			$this->controllers = array('menu');
 		}
 	}
 ?>

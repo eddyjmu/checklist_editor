7eba67b4fab2c631e42fcdbb0a52d1e588af9818
mullere <edward.j.muller@gmail.com>
2015-10-24 18:33:41 -0400

    Update reportspage.class.php

--- includes/reportspage.class.php
+++ includes/reportspage.class.php
@@ -1,10 +1,18 @@
 <?php
 	class reportspage {
+		public $page_call;
 		public $page_title;
+		public $page_menu;
 		public $page_content;
+		public $dependencies;
+		public $view;
+		public $controllers;
 		function __construct(){
+			$this->page_call = 'reportspage';
 			$this->page_title = 'Reports';
 			$this->page_content = '';
+			$this->view = 'page';
+			$this->controllers = array('menu');
 		}
 	}
 ?>

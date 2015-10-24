32677eca2ecc3cfa8bc5748cf8302f9836533c4c
mullere <edward.j.muller@gmail.com>
2015-10-24 18:40:40 -0400

    Create statusmessage.class.php

---
+++ includes/statusmessage.class.php
@@ -0,0 +1,14 @@
+<?php
+
+	class statusmessage {
+		private $page_model;
+		public function __construct($page_model){
+			$this->page_model = $page_model;
+			if($this->page_model->save_status!=0){
+				$status_message = settings::$status_array[$this->page_model->save_status];
+			} else {
+				$status_message = '';
+			}
+			$this->page_model->page_content .= '<b>'.$status_message.'</b><br /><br />';
+		}
+	}

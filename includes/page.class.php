cff3de9f22a75cb288615e1c217988db6ffaae03
mullere <edward.j.muller@gmail.com>
2015-10-24 18:33:04 -0400

    Update page.class.php

--- includes/page.class.php
+++ includes/page.class.php
@@ -1,30 +1,14 @@
 <?php
 	class page {
-		public $model;
-		public $page_sought;
-		public function __construct($page_sought,$model){
-			$this->model = $model;
-			$this->page_sought = $page_sought;
-		}
-		public function menu(){
-			$menu = '';
-			foreach(settings::$page_arrays as $page_get => $page_array){
-				$menu .= '<a href="?page='.$page_get.'">';
-				if($page_get == $this->page_sought){
-					$menu .= '['.$page_array['title'].']';
-				} else {
-					$menu .= $page_array['title'];
-				}
-				$menu .= '</a> ';
-			}
-			$menu .= '';
-			return $menu;
+		private $page_model;
+		public function __construct($page_model){
+			$this->page_model = $page_model;
 		}
 		public function output(){
-			$page_content = '<html><head><title>'.$this->model->page_title.' | '.settings::$site_title.'</title><head><body>';
+			$page_content = '<html><head><title>'.$this->page_model->page_title.' | '.settings::$site_title.'</title><head><body>';
 			$page_content .= '<h3>'.settings::$site_title.'</h3>';
-			$page_content .= $this->menu().'</br></br>';
-			$page_content .= $this->model->page_content;
+			$page_content .= $this->page_model->page_menu.'</br></br>';
+			$page_content .= $this->page_model->page_content;
 			return $page_content;
 		}
 	}

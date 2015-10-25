2267482578d559a33f6f7e876ad329a6a9e55e2a
mullere <edward.j.muller@gmail.com>
2015-10-25 16:56:50 -0400

    Update settings.class.php

--- includes/settings.class.php
+++ includes/settings.class.php
@@ -6,6 +6,10 @@
 		public static $xml_folder;
 		public static $status_array;
 		public static $db_information;
+		public static $css_folder;
+		public static $js_folder;
+		public static $default_css;
+		public static $default_js;
 		public function __construct(){
 			self::$page_arrays = array(
 				'editlistpage' => 'Lists',
@@ -13,16 +17,20 @@
 				//'optionspage' => 'Options'
 				'homepage' => 'About',
 			);
-			self::$includes_folder = 'includes';
+			self::$includes_folder = 'includes';	// just realized that i forgot to add the / and it's lke throughout the entire project now
 			self::$site_title = 'Checklist Editor';
 			self::$xml_folder = 'checklists';
-			self::$status_array = array('Save failed!','That checklist already exists!','Checklist saved!','Checklist deleted!');
+			self::$status_array = array('save failed!','that checklist already exists!','checklist saved!','checklist deleted!');
 			self::$db_information = array(
 				'DB_HOST' => 'localhost',
 				'DB_USER' => 'root',
-				'DB_PSWD' => 'RACKSPACEISAWESOME',		// please please remember to change this from the page when you upload to git
+				'DB_PSWD' => 'R0ckspac3R0cks',		// please please remember to change this from the page when you upload to git
 				'DB_NAME' => 'checklist_editor'
 			);
+			self::$css_folder = 'css/';
+			self::$js_folder = 'misc/js/';
+			self::$default_js = array('https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js');
+			self::$default_css = array(self::$css_folder.'style.css');
 		}
 	}
 ?>

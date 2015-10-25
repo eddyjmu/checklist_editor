0705b1cb653875d90b7a766f4f0c7a2c60e928fb
mullere <edward.j.muller@gmail.com>
2015-10-25 16:52:27 -0400

    Update homepage.class.php

--- includes/homepage.class.php
+++ includes/homepage.class.php
@@ -6,13 +6,17 @@
 		public $page_content;
 		public $dependencies;
 		public $view;
-		public $controllers;
+		public $controllers;		
+		public $page_css;
+		public $page_js;
 		function __construct(){
 			$this->page_call = 'homepage';
 			$this->page_title = settings::$page_arrays[$this->page_call];
 			$this->page_content = 'Hello and welcome to Checklist Editor.</br><i>Edward Muller - October 2015</i>';
 			$this->view = 'page';
 			$this->controllers = array('menu');
+			$this->page_js = settings::$default_js;
+			$this->page_css = settings::$default_css;
 		}
 	}
 ?>

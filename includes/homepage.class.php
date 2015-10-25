03ddcbac8ccd82502d893e9c35d0a172ecb3674c
mullere <edward.j.muller@gmail.com>
2015-10-25 12:38:24 -0400

    Update homepage.class.php

--- includes/homepage.class.php
+++ includes/homepage.class.php
@@ -9,7 +9,7 @@
 		public $controllers;
 		function __construct(){
 			$this->page_call = 'homepage';
-			$this->page_title = 'Home';
+			$this->page_title = settings::$page_arrays[$this->page_call];
 			$this->page_content = 'Hello and welcome to Checklist Editor.</br><i>Edward Muller - October 2015</i>';
 			$this->view = 'page';
 			$this->controllers = array('menu');

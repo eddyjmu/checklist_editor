a1de93d2faf2a45f12177b6fca380fd7be89b4b2
mullere <edward.j.muller@gmail.com>
2015-10-25 12:46:00 -0400

    Update savelist.class.php

--- includes/savelist.class.php
+++ includes/savelist.class.php
@@ -4,19 +4,21 @@
 		private $page_model;
 		public function __construct($page_model){
 			$this->page_model = $page_model;
-			$xml_object = new xml();
+			$db_object = new db();
 			//check if editing list or new list
 			if($this->page_model->new_checklist){
-				// if creating new list, check if file exists or not
-				if($xml_object->check_list_exists($_POST['checklist'])){
+				// if creating new list, check if list exists or not
+				if($db_object->check_list_exists($_POST['checklist'])){
 					$status = 1;
-				} else {
-					$xml_object->create_list_file($_POST);
+				}
+				// list does not yet exist
+				else {
+					$db_object->create_list($_POST);
 					$status = 2;
 				}
 			} else {
 				// if editing list only
-				if($xml_object->save_to_list($_POST)){
+				if($db_object->save_to_list($_POST)){
 					$status = 2;
 				} else {
 					$status = 0;

495e794e8800c4855063f457383f11f3619cfe64
mullere <edward.j.muller@gmail.com>
2015-10-25 16:52:57 -0400

    Update listsaveform.class.php

--- includes/listsaveform.class.php
+++ includes/listsaveform.class.php
@@ -35,17 +35,19 @@
 			$this->page_model->page_content .= $this->build_form_string($form_array, $list_name, $this->page_model->new_checklist);
 		}
 		function build_form_string($form_array, $list_name, $new_checklist){
-			$form_string = '<form method="post" action="index.php?page=';
+			$form_string = '<div id="list_save_form" class="content_filler"><form method="post" action="index.php?page=';
 				$form_string .= 'editlistpage';
-			$form_string .= '">';
+			$form_string .= '"><a id="content_title">';
 			if (($list_name<>'')&&($this->page_model->save_status!=1)) {
-				settings::$page_arrays[$this->page_model->page_call] = 'New List';
-				$form_string .= 'Editing checklist "'.$list_name.'"<input type="hidden" name="checklist" value="'.$list_name.'">';
+				settings::$page_arrays[$this->page_model->page_call] = '(New List)';
+				$form_string .= 'Editing checklist <span id="list_name">'.$list_name.'</span><input type="hidden" name="checklist" value="'.$list_name.'">';
 			} else {
-				$form_string .= 'New checklist: <input type"text" placeholder="checklist name" name="checklist">';
+				$form_string .= 'New checklist<input class="new_list" type"text" placeholder="checklist name" name="checklist" size="40">';
 			}
+			$form_string .= '</a>';
+			$form_string .= '<div id="checklist_items"><a id="items_title">Checklist items:</a>';
 			$form_string .= '<input type="hidden" value="'.$form_array['creation'].'" name="checklist_uts">';
-			$form_string .= '<ul>';
+			$form_string .= '<ul id="list_items">';
 			$i=0;
 			foreach($form_array['items'] as $item_id => $item){
 				if(!isset($item['completed'])){
@@ -55,40 +57,50 @@
 				} elseif ($item['completed']!=0){
 					$completed_value = $item['completed'];
 					$checked = 'checked';
-					$completion = ' <i>completion: '.date('M jS, o @ g:i a', $item['completion']).'</i>';
+					//$completion = '(completed '.date('M jS, o @ g:i a', $item['completion']).')';
+					$completion = ' completed';
 				} else {
 					$completed_value = '';
 					$checked = '';
 					$completion = '';
 				}
 				if($item['description']!=null){
-					$form_string .= '<li><input type="checkbox" name="checklist_item_'.$item_id.'_completed" '.$checked.' value="'.$completed_value.'">';
-					$form_string .= '<input type="text" name="checklist_item_'.$item_id.'_description" value="'.$item['description'].'"> ';
+					$form_string .= '<li class="occupied'.$completion.'" id="list_item_'.$item_id.'">';
+					$form_string .= '<input class="checkbox" type="checkbox" name="checklist_item_'.$item_id.'_completed" '.$checked.' value="'.$completed_value.'">';
+					$form_string .= '<span class="list_item_text">'.$item['description'].'</span>';
+					$form_string .= '<input class="item_box hidden" type="text" name="checklist_item_'.$item_id.'_description" value="'.$item['description'].'">';
+				} else {
 				}
-				if(!$new_checklist){
-					$form_string .= $this->modifiers('-');
-				}
-				$form_string .= $completion.'</li>';
+				//$form_string .= '<span class="completion_date">'.$completion.'</span></li>'; // forgot to pass the old completion values through the form and then into db, so completion date is always when the list is saved -_-
+				$form_string .= '</li>';
 				$i++;
 			}
 			if($i>0){
-				$form_string .= '<li><input type="checkbox" name="checklist_item_'.$this->free_item_id.'_completed">';
-				$form_string .= '<input type="text" name="checklist_item_'.$this->free_item_id.'_description" placeholder="new list item">';
-				$form_string .= $this->modifiers('+');
+				$form_string .= '<li class="new"><input class="checkbox" type="checkbox" name="checklist_item_'.$this->free_item_id.'_completed">';
+				$form_string .= '<input class="item_box" type="text" name="checklist_item_'.$this->free_item_id.'_description" placeholder="new list item">';
 			}
-			$form_string .= '</ul><input type="submit" value="Save Checklist" name="commit_';
+			$form_string .= '</ul>';
+			$form_string .= '</div>';
+			$form_string .= '<div id="commits">';
 			if($new_checklist){
-				$form_string .= 'new_';
+				$form_string .= '<input type="submit" value="Save new checklist" name="commit_new_save">';
+			} else {
+				$form_string .= '<input type="submit" value="Save checklist" name="commit_save">';
 			}
-			$form_string .= 'save">     ';
+			$form_string .= '</div>';
 			if(!$new_checklist){
 				//$form_string .= '<input type="submit" value="Delete Checklist" name="commit_delete">';
 			}
-			$form_string .= '</form>';
+			$form_string .= '</form></div>';
 			return $form_string;
 		}
-		function modifiers($type){
-			$up_down = '<a href="#" class="">[up]</a><a href="#" class="">[down]</a>';
+		function modifiers($type,$add_up_down){
+			if($add_up_down){
+				$up_down = '<a href="#" class="">[up]</a><a href="#" class="">[down]</a>';
+			} else {
+				$up_down = '';
+			}
+			
 			if($type=='+'){
 				return '<a href="#" class="">[+]</a>'.$up_down;
 			} else {

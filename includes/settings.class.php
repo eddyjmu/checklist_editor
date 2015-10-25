259ee03bf47ce29a01fff7745ae33027e27635cc
mullere <edward.j.muller@gmail.com>
2015-10-25 12:47:27 -0400

    Update settings.class.php

--- includes/settings.class.php
+++ includes/settings.class.php
@@ -5,17 +5,24 @@
 		public static $site_title;
 		public static $xml_folder;
 		public static $status_array;
+		public static $db_information;
 		public function __construct(){
 			self::$page_arrays = array(
-				'homepage' => 'Home',
 				'editlistpage' => 'Lists',
-				'reportspage' => 'Reports',
-				'optionspage' => 'Options'
+				//'reportspage' => 'Reports',
+				//'optionspage' => 'Options'
+				'homepage' => 'About',
 			);
 			self::$includes_folder = 'includes';
 			self::$site_title = 'Checklist Editor';
 			self::$xml_folder = 'checklists';
-			self::$status_array = array('Save Failed!','Checklist Name already exists!','Checklist Saved!','Checklist Deleted!');
+			self::$status_array = array('Save failed!','That checklist already exists!','Checklist saved!','Checklist deleted!');
+			self::$db_information = array(
+				'DB_HOST' => 'localhost',
+				'DB_USER' => 'root',
+				'DB_PSWD' => 'Living4me',		// please please remember to change this from the page when you upload to git
+				'DB_NAME' => 'checklist_editor'
+			);
 		}
 	}
 ?>

4713624029e4ec47d4c8352d6b2e7f7522381705
mullere <edward.j.muller@gmail.com>
2015-10-26 21:56:26 -0400

    Update jquerymod.js

--- misc/js/jquerymod.js
+++ misc/js/jquerymod.js
@@ -12,14 +12,14 @@ $(document).ready(function(){
 	});
 	$('input.checkbox').click(function(){
 		$(this).parent().toggleClass('completed');
-	})
+	});
 	$('#addrow').click(function(){
 		$('#list_items').append('<li class="new" id="'+new_id()+'" data-id="'+new_id()+'"><input class="checkbox" type="checkbox" name="checklist_item_'+new_id()+'_completed"><div class="list_item_content"><textarea class="item_box" name="checklist_item_'+new_id()+'_description" placeholder="new list item"></textarea></div>');
 	});
-	/*$('a.updn').click(function(){
-		var $self_li, $target_li, direction, target_id, temp_value, temp_completion, temp_check;
-		$self_li = $(this).parent().parent();
-		direction = $(this).attr('data-direction');
+	$('a.move').click(function(){
+		var target_id, temp_value;
+		var $self_li = $(this).parent().parent();
+		var direction = $(this).attr('data-direction');
 		if (direction == 'up') {
 			target_id = $self_li.prev().attr('data-id');
 		} else if (direction == 'dn') {
@@ -27,19 +27,31 @@ $(document).ready(function(){
 		} else {
 			// do nothing because never called
 		}
-		$target_li = $self_li.parent().children('li[data-id="'+target_id+'"]');
-		// temp = target
-		temp_value = $target_li.children('div textarea').val();
-		temp_completion = $target_li.children('input').val();
-		temp_check = $target_li.children('input').checked;
-		// target= self
-		$target_li.children('div textarea').val( $self_li.children('div textarea').val() );
-		$target_li.children('input').val( $self_li.children('input').val()					);
-		$target_li.children('input').prop('checked', $self_li.children('input').checked );
-		// self = temp
-		$self_li.children('div textarea').val( temp_value );
-		$self_li.children('input').val( temp_completion );
+		var $target_li = $self_li.parent().children('li[data-id="'+target_id+'"]');
+		var temp_value = $target_li.children('div.list_item_content').children('textarea').val();
+		var temp_check = $target_li.children('input.checkbox').prop('checked');
+		var self_value = $self_li.children('div.list_item_content').children('textarea').val();
+		$target_li.children('div.list_item_content').children('span').html( self_value );
+		$target_li.children('div.list_item_content').children('textarea').val( self_value );
+		$target_li.children('input.checkbox').prop('checked', $self_li.children('input.checkbox').prop('checked') );
+		$self_li.children('div.list_item_content').children('textarea').val( temp_value );
+		$self_li.children('div.list_item_content').children('span').html( temp_value );
 		$self_li.children('input').prop('checked', temp_check );
+		$('input[type="checkbox"]').each(function(i){
+			$(this).parent().removeClass('completed');
+			if($(this).prop('checked')){
+				$(this).parent().addClass('completed');
+			}		
+		});
+	});
+	// an attempt to make every list item's done checkmark disappear if nothing is in the textarea 
+	/*$('li').each(function(i){
+		var $content_box = $(this).children('div.list_item_content');
+		if(!$content_box.children('textarea').val() == ''){
+			$content_box.children('a').html('done&check;');
+		} else {
+			$content_box.children('a').html('');
+		}
 	});*/
 });
 

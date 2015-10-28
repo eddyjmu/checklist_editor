66260d8a7a817ab9a43ad6674481d7611aa016b2
mullere <edward.j.muller@gmail.com>
2015-10-28 08:23:14 -0400

    Update homepage.class.php

--- includes/homepage.class.php
+++ includes/homepage.class.php
@@ -18,6 +18,7 @@
 			<div id="checklist_items">Included features:<ul id="list_items">
 			<li><u>Checklist Creation:</u><br /> User is able to create a checklist (provided that the name chosen is not already taken) and add items to that list directly from the front-page.</li>
 			<li><u>Checklist Loading:</u><br /> User is able to load any created checklists via the [Load Checklists] section at the top of every page. </li>
+			<li><u>Checklist Deletion:</ul><br />User is able to make use of the [Delete Checklist] button (to the right of the [Save Checklist] button) which will remove the checklist from the listing upon confirmation.</li>
 			<li><u>Checklist Item Completion:</u><br /> User is able to check off items in checklists.*</li>
 			<li><u>Checklist Editing:</u><br /> User is able to edit the contents of each list item by clicking on the text (which will change color when hovered over) and clicking [&check;done] when finished.*</li>
 			<li><u>Checklist Order Modification:</u><br /> User is able to adjust the order of the list items in each checklist by clicking on either of the arrows provided. CLicking [&#8613] will flip the item with the one above it, and [&#8615] will flip the item with the one below it.*</li>

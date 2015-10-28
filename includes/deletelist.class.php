721566a0410e58739d20fc1ee26bec865f1182be
mullere <edward.j.muller@gmail.com>
2015-10-27 21:51:20 -0400

    Update deletelist.class.php

--- includes/deletelist.class.php
+++ includes/deletelist.class.php
@@ -4,8 +4,8 @@
 		private $page_model;
 		public function __construct($page_model){
 			$this->page_model = $page_model;
-			$xml_object = new xml();
-			$xml_object->delete_list_file($_POST['checklist']);
+			$db_object = new db();
+			$db_object->delete_list($_POST);
 			$this->page_model->save_status = 3;
 		}
 	}
